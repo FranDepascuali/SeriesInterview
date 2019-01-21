09c66572802b4efbbeca6c485afbde4608f12e56
Francisco Depascuali <francisco.depascuali@gmail.com>
2019-01-21 20:18:29 -0300

    Exctract date formatter to its own extension

---
+++ Series/Extensions/DateFormatter.swift
@@ -0,0 +1,20 @@
+//
+//  DateFormatter.swift
+//  Series
+//
+//  Created by Francisco Depascuali on 21/01/2019.
+//  Copyright © 2019 depa. All rights reserved.
+//
+
+import Foundation
+
+extension DateFormatter {
+    static let yyyyMMdd: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
+}
